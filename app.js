fb51217e4c797c357c4567035629928bf39c1a28
JenniD5 <elizabethduran0105@gmail.com>
2019-09-18 09:07:13 -0700

    modificacion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const pokemonName=document.getElementById(`pokemon-name`);
+/*const pokemonName=document.getElementById(`pokemon-name`);
 //pokemonName.innerText=`El nombre de mi pokemon es: `;  
 const pokemonheight=document.getElementById(`pokemonheight`);
+const pokemonFrontImg = document.getElementById('pokemon-img-front');
 
 const pokemonApiUrl=`https://pokeapi.co/api/v2/`;
 
@@ -9,11 +10,32 @@ let getPokemonData=async ()=>{
     const pokemon= await respose.json();
     console.log(pokemon);
     const {name, height}= pokemon;
+    const frontImg = pokemon.sprites.front_default;
     pokemonName.innerText=name;
     pokemonheight.innerText=`la altura es ${height}`;
+    pokemonFrontImg.src = frontImg;
 }
 
-getPokemonData();
+getPokemonData();*/
 //async (asincrona) hace una busqueda en segundo plano, let es para hacer una funcion
 //await para esperar a que haga la busqueda 
 //fetch es como un foreach, y necesita una url para ir  abuscar 
+
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonHeight = document.getElementById('pokemon-height');
+const pokemonFrontImg = document.getElementById('pokemon-img-front');
+
+const pokemonApiUrl = 'https://pokeapi.co/api/v2/';
+
+let getPokemonData = async ()=> {
+    const respose = await fetch(`${pokemonApiUrl}pokemon/1`);
+    const pokemon = await respose.json();
+    console.log(pokemon);
+    const {name , height} = pokemon;
+    const frontImg = pokemon.sprites.front_default;
+    pokemonName.innerText = name;
+    pokemonHeight.innerText = `La altura es: ${height}`;
+    pokemonFrontImg.src = frontImg;
+};
+
+getPokemonData();
