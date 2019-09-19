239d358626fb1dc7be1bd047572f61ba56411696
JenniD5 <elizabethduran0105@gmail.com>
2019-09-18 22:46:08 -0700

    pokemonn

--- app.js
+++ app.js
@@ -24,18 +24,28 @@ getPokemonData();*/
 const pokemonName = document.getElementById('pokemon-name');
 const pokemonHeight = document.getElementById('pokemon-height');
 const pokemonFrontImg = document.getElementById('pokemon-img-front');
+const pokemonNumber = document.getElementById('pokemon-number');
+const pokemonWeight=document.getElementById('pokemon-weight');
+pokemonAbilities=document.getElementById(``);
 
 const pokemonApiUrl = 'https://pokeapi.co/api/v2/';
 
 let getPokemonData = async ()=> {
-    const respose = await fetch(`${pokemonApiUrl}pokemon/1`);
+    const respose = await fetch(`${pokemonApiUrl}pokemon/67`);
     const pokemon = await respose.json();
     console.log(pokemon);
-    const {name , height} = pokemon;
+    const {name , height, abilities} = pokemon;
     const frontImg = pokemon.sprites.front_default;
     pokemonName.innerText = name;
     pokemonHeight.innerText = `La altura es: ${height}`;
+    pokemonNumber.innerText=`numero en la lista: ${pokemon.id}`;
+    pokemonWeight.innerText=`el es de: ${pokemon.weight}`;
     pokemonFrontImg.src = frontImg;
+abilities.forEach(element => {
+    pokemonAbilities.innerHTML=`<li> ${element.name} </li>`;
+    
+});
+
 };
 
 getPokemonData();
